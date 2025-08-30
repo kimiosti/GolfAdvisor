6f1fb8a5dfb707d191d27a6fced7c338eb9e1f77
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-30 11:49:01 +0200

    Modified main activity do create navigation controller, used to properly show the top bar and the application screens

--- app/src/main/java/com/example/golfadvisor/MainActivity.kt
+++ app/src/main/java/com/example/golfadvisor/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.golfadvisor.ui.navigation.NavGraph
+import com.example.golfadvisor.ui.screens.commons.TopBar
 import com.example.golfadvisor.ui.theme.GolfAdvisorTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GolfAdvisorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                val navController = rememberNavController()
+                Scaffold(
+                    topBar = { TopBar(navController) },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    NavGraph(
+                        navController = navController,
+                        logged = false, /* TODO - add check for login on startup */
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GolfAdvisorTheme {
-        Greeting("Android")
     }
 }
